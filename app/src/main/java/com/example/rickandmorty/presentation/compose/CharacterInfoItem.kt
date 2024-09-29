69d1842ae15a6ba90460889d1e0c1ecf2a2e4c4c
MatLeg <mat.legierskii@gmail.com>
2024-09-29 19:47:10 +0200

    update CharacterInfoItem to handle isFavourite state

--- app/src/main/java/com/example/rickandmorty/presentation/compose/CharacterInfoItem.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/compose/CharacterInfoItem.kt
@@ -1,25 +1,47 @@
 package com.example.rickandmorty.presentation.compose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.rickandmorty.R
 
 @Composable
 @Preview
 fun CharacterInfoItem(
     modifier: Modifier = Modifier,
     avatarUrl: String = "",
-    name: String = "Name"
+    name: String = "Name eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeee",
+    isFavourite: Boolean = false //todo implement favourites
 ) {
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-        Image(imageVector = Icons.Default.AccountBox, contentDescription = "desc")
-        Text(text = name)
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            imageVector = Icons.Default.AccountBox,
+            contentDescription = stringResource(id = R.string.character_avatar)
+        )
+        Text(
+            text = name,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(1f)
+        )
+        Image(
+            imageVector = Icons.Default.FavoriteBorder,
+            contentDescription = stringResource(id = R.string.favourite)
+        )
     }
 }
