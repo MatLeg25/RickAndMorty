975db9f19c1f21395674bda001991c9f930afd62
MatLeg <mat.legierskii@gmail.com>
2024-09-29 18:16:34 +0200

    add CharactersViewModel

--- app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersScreen.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 import com.example.rickandmorty.Greeting
 
 @Composable
---
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.rickandmorty.presentation.characters
+
+import androidx.lifecycle.ViewModel
+
+class CharactersViewModel: ViewModel() {
+}
