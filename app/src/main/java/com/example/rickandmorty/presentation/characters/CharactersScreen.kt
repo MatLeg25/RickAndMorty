8c37cf318010e1c7c9d0f20475e87192f584d59f
MatLeg <mat.legierskii@gmail.com>
2024-09-29 18:15:07 +0200

    update base structure, add CharactersScreen

---
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersScreen.kt
@@ -0,0 +1,16 @@
+package com.example.rickandmorty.presentation.characters
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import com.example.rickandmorty.Greeting
+
+@Composable
+fun CharactersScreen(modifier: Modifier = Modifier, innerPadding: PaddingValues) {
+    Greeting(
+        name = "Android",
+        modifier = Modifier.padding(innerPadding)
+    )
+}
