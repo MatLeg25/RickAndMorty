597b13f9da66ae45476907f1951ace8fc05e86e1
MatLeg <mat.legierskii@gmail.com>
2024-10-01 19:02:58 +0200

    move ChangeScreenModeBtn to be always on the top

--- app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersScreen.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersScreen.kt
@@ -1,8 +1,10 @@
 package com.example.rickandmorty.presentation.characters
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -25,54 +27,61 @@ fun CharactersScreen(
     val state = viewModel.state.value
     val items = if (state.screenMode == ScreenMode.ALL) state.characters else state.favourites
 
-    LazyColumn(modifier = modifier.padding(innerPadding)) {
-        item {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                ChangeScreenModeBtn(
-                    modifier = Modifier,
-                    btnTextResId =  R.string.all,
-                    isSelected = state.screenMode == ScreenMode.ALL,
-                    onClick = {
-                        viewModel.onEvent(
-                            CharactersScreenEvent.ChangeScreenMode(
-                                ScreenMode.ALL
-                            )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            ChangeScreenModeBtn(
+                modifier = Modifier,
+                btnTextResId = R.string.all,
+                isSelected = state.screenMode == ScreenMode.ALL,
+                onClick = {
+                    viewModel.onEvent(
+                        CharactersScreenEvent.ChangeScreenMode(
+                            ScreenMode.ALL
                         )
-                    }
-                )
-                ChangeScreenModeBtn(
-                    modifier = Modifier,
-                    btnTextResId =  R.string.favourite,
-                    isSelected = state.screenMode == ScreenMode.FAVOURITES,
-                    onClick = {
-                        viewModel.onEvent(
-                            CharactersScreenEvent.ChangeScreenMode(
-                                ScreenMode.FAVOURITES
-                            )
+                    )
+                }
+            )
+            ChangeScreenModeBtn(
+                modifier = Modifier,
+                btnTextResId = R.string.favourite,
+                isSelected = state.screenMode == ScreenMode.FAVOURITES,
+                onClick = {
+                    viewModel.onEvent(
+                        CharactersScreenEvent.ChangeScreenMode(
+                            ScreenMode.FAVOURITES
                         )
-                    }
-                )
-            }
-        }
-        items(items) {
-            CharacterInfoItem(
-                modifier = Modifier.fillMaxWidth(),
-                name = it.name,
-                avatarUrl = it.avatarUrl,
-                isFavourite = it.isFavourite,
-                onFavoriteClick = {
-                    viewModel.onEvent(CharactersScreenEvent.AddDeleteFavourite(it))
+                    )
                 }
             )
         }
-        item {
-            LoadMore(
-                isMoreData = state.isMoreData,
-                loadMoreFun = viewModel::fetchData
-            )
+
+        LazyColumn(modifier = Modifier) {
+            items(items) {
+                CharacterInfoItem(
+                    modifier = Modifier.fillMaxWidth(),
+                    name = it.name,
+                    avatarUrl = it.avatarUrl,
+                    isFavourite = it.isFavourite,
+                    onFavoriteClick = {
+                        viewModel.onEvent(CharactersScreenEvent.AddDeleteFavourite(it))
+                    }
+                )
+            }
+            item {
+                LoadMore(
+                    isMoreData = state.isMoreData,
+                    loadMoreFun = viewModel::fetchData
+                )
+            }
         }
     }
+
+
 }
