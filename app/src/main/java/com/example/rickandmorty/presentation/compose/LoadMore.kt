0e5ad320f026104e34d40cc847e3732770cd95e9
MatLeg <mat.legierskii@gmail.com>
2024-10-03 18:16:57 +0200

    add padding

--- app/src/main/java/com/example/rickandmorty/presentation/compose/LoadMore.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/compose/LoadMore.kt
@@ -3,10 +3,12 @@ package com.example.rickandmorty.presentation.compose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadMore(
@@ -18,7 +20,9 @@ fun LoadMore(
             loadMoreFun()
         }
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
             horizontalArrangement = Arrangement.Center
         ) {
             CircularProgressIndicator()
